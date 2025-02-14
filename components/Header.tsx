ecfcb6b9818f347019bf2a12785dd3eed2a76018
OliverJueguen <oliverjueguen@gmail.com>
2025-02-14 20:11:18 +0100

    Fixed header2

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { useTheme } from '@/context/ThemeContext'
+import { useTheme } from '../context/ThemeContext'
 
 const Header: FC = () => {
   const { isDarkMode, toggleTheme } = useTheme()
