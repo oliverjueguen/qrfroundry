3f1110956a9c2f1f2372bc6eefdee0152735657a
OliverJueguen <oliverjueguen@gmail.com>
2025-02-14 20:59:47 +0100

    fix: reposition GTM script to match official implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,22 +20,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* Google Tag Manager Script - placed in head */}
-      <Script
-        id="gtm-script"
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <head>
+        {/* Google Tag Manager - placed at the very top of head */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TJCXSTTG');
-          `
-        }}
-      />
-      <head />
+            })(window,document,'script','dataLayer','GTM-TJCXSTTG');`
+          }}
+        />
+      </head>
       <body className={inter.className}>
         {/* Google Tag Manager NoScript - placed at start of body */}
         <noscript>
